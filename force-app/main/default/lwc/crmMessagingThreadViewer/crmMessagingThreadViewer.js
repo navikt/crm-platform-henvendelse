477b8cc12c6cc779d5be63fa47075fe7df430d5d
Lars Petter Johnsen <lars.petter.johnsen@accenture.com>
2021-08-04 08:41:55 +0200

    Added support for Quicktext

--- force-app/main/default/lwc/crmMessagingThreadViewer/crmMessagingThreadViewer.js
+++ force-app/main/default/lwc/crmMessagingThreadViewer/crmMessagingThreadViewer.js
@@ -18,6 +18,7 @@ export default class messagingThreadViewer extends LightningElement {
     threadheader;
     threadid;
     messages = [];
+    text;
     //Constructor, called onload
     connectedCallback() {
         if (this.thread) {
@@ -44,18 +45,16 @@ export default class messagingThreadViewer extends LightningElement {
         } else if (result.data) {
             this.messages = result.data;
             this.showspinner = false;
-            this.setheader();
+
         }
     }
     //If empty, stop submitting.
     handlesubmit(event) {
-        console.log('submit');
         event.preventDefault();
         const textInput = event.detail.fields;
         // If messagefield is empty, stop the submit
         textInput.CRM_Thread__c = this.thread.Id;
         textInput.CRM_From__c = userId;
-
         if (textInput.CRM_Message_Text__c == null || textInput.CRM_Message_Text__c == '') {
             const event1 = new ShowToastEvent({
                 title: 'Message Body missing',
@@ -85,7 +84,7 @@ export default class messagingThreadViewer extends LightningElement {
         const threadInput = { fields };
 
         updateRecord(threadInput)
-            .then(() => {})
+            .then(() => { })
             .catch((error) => {
                 console.log(JSON.stringify(error, null, 2));
             });
@@ -95,13 +94,16 @@ export default class messagingThreadViewer extends LightningElement {
         console.log('Success start');
         this.recordId = event.detail;
 
+        this.template.querySelector('c-crm-messaging-quick-text').clear(event);
         const inputFields = this.template.querySelectorAll('.msgText');
+
         if (inputFields) {
             inputFields.forEach((field) => {
                 field.reset();
             });
         }
         return refreshApex(this._mySendForSplitting);
+
     }
 
     get journalEntries() {
@@ -116,5 +118,11 @@ export default class messagingThreadViewer extends LightningElement {
         return !getFieldValue(this.wiredThread.data, ACTIVE_FIELD);
     }
 
-    setheader() {}
+    showQuickText(event) {
+        this.template.querySelector('c-crm-messaging-quick-text').showModal(event);
+    }
+    handleConversationNoteChange(event) {
+        this.text = event.detail;
+
+    }
 }
