7af2e80e2f194b541be272668402c7b9fec502d7
Tor Håkon Sigurdsen <55088511+torhakon@users.noreply.github.com>
2021-11-15 13:34:47 +0100

    Updated Redact text component

--- force-app/main/default/lwc/crmRedactText/crmRedactText.js
+++ force-app/main/default/lwc/crmRedactText/crmRedactText.js
@@ -2,41 +2,89 @@ import { LightningElement, api } from 'lwc';
 
 export default class CrmRedactText extends LightningElement {
     _originalValue = '';
-    _redactedValue = '';
-    _textAreaclass = 'slds-textarea slds-text-color_default';
+    _changes = [];
+    _changesPosition = -1;
+    _textAreaclass = 'redactTextArea slds-textarea slds-text-color_default';
+    _textAreaStyle = 'resize: none; overflow: hidden; overflow:hidden;';
+    hasRendered = false;
 
     @api get textToRedact() {
         return this._originalValue;
     }
 
     set textToRedact(value) {
-        if (value) {
-            this._originalValue = value;
-            this.redactedValue = value;
-        } else {
-            throw new Error('Need original value!');
-        }
+        this.resetTextToRedact(value);
     }
 
-    set textAreaclass(value) {
-        this._textAreaclass = value;
+    set textAreaClass(value) {
+        this._textAreaclass = value ? `redactTextArea ${value}` : 'redactTextArea';
     }
 
-    @api get textAreaclass() {
+    @api get textAreaClass() {
         return this._textAreaclass;
     }
 
-    get redactedValue() {
-        return this._redactedValue;
+    @api get redactedText() {
+        return this.redactedValue;
     }
 
-    set redactedValue(value) {
-        this._redactedValue = value;
+    get redactedValue() {
+        return this._changes[this._changesPosition] ? this._changes[this._changesPosition] : this.textToRedact;
+    }
+
+    renderedCallback() {
+        if (this.hasRendered === false) {
+            this.hasRendered = true;
+            const element = this.template.querySelector('.redactTextArea');
+            element.style = `${this._textAreaStyle} height:${element.scrollHeight}px;`;
+        }
+    }
+
+    @api undoChanges() {
+        if (this.canUndo) {
+            this._changesPosition--;
+        }
+    }
+
+    @api redoChanges() {
+        if (this.canRedo) {
+            this._changesPosition++;
+        }
+    }
+
+    @api reset() {
+        this.resetTextToRedact(this._originalValue);
+    }
+
+    @api get canUndo() {
+        return this._changesPosition > 0;
+    }
+
+    @api get canRedo() {
+        return this._changesPosition < this._changes.length - 1;
+    }
+
+    addRedactedValue(value) {
+        if (this.canRedo) {
+            this._changes.length = this._changesPosition + 1;
+        }
+        this._changes.push(value);
+
+        console.log(this._changes);
+
+        this._changesPosition = this._changes.length - 1;
         this.dispatchRedactedEvent();
     }
 
-    @api cancel() {
-        this.redactedValue = this._originalValue;
+    resetTextToRedact(value) {
+        if (value) {
+            this._changes = [];
+            this._changesPosition = -1;
+            this._originalValue = value;
+            this.addRedactedValue(value);
+        } else {
+            throw new Error('Need original value!');
+        }
     }
 
     onSelectText(event) {
@@ -45,10 +93,9 @@ export default class CrmRedactText extends LightningElement {
         const start = event.target.selectionStart;
         const end = event.target.selectionEnd;
         const selection = event.target.value.substring(start, end);
-
         const redacted = selection.replace(/\S/g, '*');
 
-        this.redactedValue = this.redactedValue.substring(0, start) + redacted + this.redactedValue.substring(end, len);
+        this.addRedactedValue(this.redactedValue.slice(0, start) + redacted + this.redactedValue.slice(end, len));
     }
 
     dispatchRedactedEvent() {
