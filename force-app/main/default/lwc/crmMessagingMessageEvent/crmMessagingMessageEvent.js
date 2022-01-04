7833eaa75736860a259c93458002539580cc0a67
Eugenijus <eugenijus.margalikas@nav.no>
2022-01-04 10:44:22 +0100

    update date on event

--- force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
+++ force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
@@ -2,4 +2,15 @@ import { LightningElement, api } from 'lwc';
 
 export default class MessageEvent extends LightningElement {
     @api message;
+    get createdDateString(){
+        if(message.CreatedDate) return '';
+        let createdDate = new Date(message.CreatedDate);
+        let formatter = new Intl.DateTimeFormat('no',{day: 'numeric', month: 'long', year: 'numeric'});
+        let date = formatter.format(createdDate);
+        formatter = new Intl.DateTimeFormat('no',{hour: 'numeric', minute: 'numeric'});
+        let timeParts = formatter.formatToParts(createdDate);
+        let hour = timeParts.find( e => e.type === 'hour').value;
+        let minute = timeParts.find( e => e.type === 'minute').value;
+        return date + ', kl. ' + hour+ '.' + minute;
+    }
 }
