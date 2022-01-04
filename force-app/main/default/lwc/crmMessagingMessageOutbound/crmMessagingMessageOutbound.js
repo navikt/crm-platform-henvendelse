0319087b06fd2517d701acb54a03190b73729ceb
Eugenijus <eugenijus.margalikas@nav.no>
2022-01-04 13:06:04 +0100

    update in-outbound message appearance

--- force-app/main/default/lwc/crmMessagingMessageOutbound/crmMessagingMessageOutbound.js
+++ force-app/main/default/lwc/crmMessagingMessageOutbound/crmMessagingMessageOutbound.js
@@ -2,4 +2,12 @@ import { LightningElement, api } from 'lwc';
 
 export default class MessagingMessageOutbound extends LightningElement {
     @api message;
+    @api userid;
+
+    get isOwner(){
+        if (this.userid == this.message.CRM_From_User__c) {
+            return true;
+        }
+        return false;
+    }
 }
