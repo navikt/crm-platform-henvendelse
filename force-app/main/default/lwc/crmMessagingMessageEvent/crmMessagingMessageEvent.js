ba9ef9faf20b6d546e22edfde686ccbe227786e8
Eugenijus <eugenijus.margalikas@nav.no>
2022-01-05 10:54:42 +0100

    display different event types

--- force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
+++ force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
@@ -13,4 +13,26 @@ export default class MessageEvent extends LightningElement {
         let minute = timeParts.find( e => e.type === 'minute').value;
         return date + ', kl. ' + hour+ '.' + minute;
     }
+    get isEndChatEvent(){
+        /* 
+        *   return value should be depended on Message__c object Event Type, 
+        *   but for now (04.01.22) it is not available in data model
+        *   So this method shouold be changed as soon as Event Type is available
+        */
+        if(this.message.CRM_Message_Text__c.startsWith('a')) return true;
+        return false;
+    }
+    get isTransferEvent(){
+        /* 
+        *   return value should be depended on Message__c object Event Type, 
+        *   but for now (04.01.22) it is not available in data model
+        *   So this method shouold be changed as soon as Event Type is available
+        */
+       if(this.message.CRM_Message_Text__c.startsWith('S')) return true;
+        return false;
+    }
+    get isOtherEvent(){
+        if( this.isEndChatEvent && this.isTransferEvent) return false;
+        return true;
+    }
 }
