e2ab7f28698db5be5e4a6cc45a887b4fb4306bb2
Eugenijus <eugenijus.margalikas@nav.no>
2022-01-06 08:46:55 +0100

    added support for message event type

--- force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
+++ force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
@@ -2,24 +2,14 @@ import { LightningElement, api } from 'lwc';
 
 export default class MessageEvent extends LightningElement {
     @api message;
-    get createdDateString(){
-        if(!this.message.CreatedDate) return '';
-        let createdDate = new Date(this.message.CreatedDate);
-        let formatter = new Intl.DateTimeFormat('no',{day: 'numeric', month: 'long', year: 'numeric'});
-        let date = formatter.format(createdDate);
-        formatter = new Intl.DateTimeFormat('no',{hour: 'numeric', minute: 'numeric'});
-        let timeParts = formatter.formatToParts(createdDate);
-        let hour = timeParts.find( e => e.type === 'hour').value;
-        let minute = timeParts.find( e => e.type === 'minute').value;
-        return date + ', kl. ' + hour+ '.' + minute;
-    }
     get isEndChatEvent(){
         /* 
         *   return value should be depended on Message__c object Event Type, 
         *   but for now (04.01.22) it is not available in data model
         *   So this method shouold be changed as soon as Event Type is available
         */
-        if(this.message.CRM_Message_Text__c.startsWith('a')) return true;
+        if(this.message.CRM_Event_Type__c === 'END_OF_CHAT' ||
+            this.message.CRM_Message_Text__c.includes('avslutt')) return true;
         return false;
     }
     get isTransferEvent(){
@@ -28,7 +18,9 @@ export default class MessageEvent extends LightningElement {
         *   but for now (04.01.22) it is not available in data model
         *   So this method shouold be changed as soon as Event Type is available
         */
-       if(this.message.CRM_Message_Text__c.startsWith('S')) return true;
+       if(this.message.CRM_Event_Type__c === 'UNIT_TRANSFER' ||
+            this.message.CRM_Event_Type__c === 'QUEUE_TRANSFER' ||
+            this.message.CRM_Message_Text__c.startsWith('overført')) return true;
         return false;
     }
     get isOtherEvent(){
