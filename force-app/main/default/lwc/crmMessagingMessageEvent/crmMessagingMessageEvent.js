71fbb9ee99d4308904c87547ced315f2d741176b
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2022-01-11 09:41:45 +0100

    Deleted From_Last_Name and From_Integration_Name from Message

--- force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
+++ force-app/main/default/lwc/crmMessagingMessageEvent/crmMessagingMessageEvent.js
@@ -2,35 +2,42 @@ import { LightningElement, api } from 'lwc';
 
 export default class MessageEvent extends LightningElement {
     @api message;
-    get endOfChatMessage(){
+    get endOfChatMessage() {
         return 'avsluttet samtalen';
     }
-    get transferMessage(){
+    get transferMessage() {
         return this.message.CRM_Message_Text__c;
     }
-    get isEndChatEvent(){
-        /* 
-        *   return value should be depended on Message__c object Event Type, 
-        *   but for now (04.01.22) it is not available in data model
-        *   So this method shouold be changed as soon as Event Type is available
-        */
-        if(this.message.CRM_Event_Type__c === 'END_OF_CHAT' ||
-            this.message.CRM_Message_Text__c.includes('avslutt')) return true;
+    get isEndChatEvent() {
+        /*
+         *   return value should be depended on Message__c object Event Type,
+         *   but for now (04.01.22) it is not available in data model
+         *   So this method shouold be changed as soon as Event Type is available
+         */
+        if (this.message.CRM_Event_Type__c === 'END_OF_CHAT' || this.message.CRM_Message_Text__c.includes('avslutt'))
+            return true;
         return false;
     }
-    get isTransferEvent(){
-        /* 
-        *   return value should be depended on Message__c object Event Type, 
-        *   but for now (04.01.22) it is not available in data model
-        *   So this method shouold be changed as soon as Event Type is available
-        */
-       if(this.message.CRM_Event_Type__c === 'UNIT_TRANSFER' ||
+    get isTransferEvent() {
+        /*
+         *   return value should be depended on Message__c object Event Type,
+         *   but for now (04.01.22) it is not available in data model
+         *   So this method shouold be changed as soon as Event Type is available
+         */
+        if (
+            this.message.CRM_Event_Type__c === 'UNIT_TRANSFER' ||
             this.message.CRM_Event_Type__c === 'QUEUE_TRANSFER' ||
-            this.message.CRM_Message_Text__c.includes('overført')) return true;
+            this.message.CRM_Message_Text__c.includes('overført')
+        )
+            return true;
         return false;
     }
-    get isOtherEvent(){
-        if( this.isEndChatEvent || this.isTransferEvent) return false;
+    get isOtherEvent() {
+        if (this.isEndChatEvent || this.isTransferEvent) return false;
         return true;
+    }
+
+    get sender() {
+        return this.message.CRM_External_Message__c === true ? 'NAV' : this.message.CRM_From_Label__c;
     }
 }
