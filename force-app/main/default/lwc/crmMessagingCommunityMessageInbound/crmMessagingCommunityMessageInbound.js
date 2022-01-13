1d0a44446bd5589efd42c4030befbb131e2ca672
Eugenijus <eugenijus.margalikas@nav.no>
2022-01-13 14:13:11 +0100

    hiding name and cleanup

--- force-app/main/default/lwc/crmMessagingCommunityMessageInbound/crmMessagingCommunityMessageInbound.js
+++ force-app/main/default/lwc/crmMessagingCommunityMessageInbound/crmMessagingCommunityMessageInbound.js
@@ -1,18 +1,8 @@
 import { LightningElement, api } from 'lwc';
-import logos from '@salesforce/resourceUrl/stoLogos';
+import logos from '@salesforce/resourceUrl/stoLogos/navLogoRed.svg';
 
 export default class CommunityMessageInbound extends LightningElement {
     @api message;
-    showNavlogo;
-    navlogo = logos + '/navLogoRed.svg';
+    navlogo = logos;
 
-    connectedCallback() {
-        if (this.message.CRM_From_User__c && this.message.CRM_From_User__c !== '') {
-            this.showNavlogo = true;
-        }
-    }
-
-    get sender() {
-        return this.message.CRM_External_Message__c === true ? 'NAV' : this.message.CRM_From_Label__c;
-    }
 }
