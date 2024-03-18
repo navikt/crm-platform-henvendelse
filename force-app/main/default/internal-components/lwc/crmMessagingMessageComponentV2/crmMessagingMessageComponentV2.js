d05f9b379d2eeebd7dd97db8b2318d98c7ac9e27
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2024-03-18 23:51:01 +0100

    added new version of messaging cmps (related to nks new design)

---
+++ force-app/main/default/internal-components/lwc/crmMessagingMessageComponentV2/crmMessagingMessageComponentV2.js
@@ -0,0 +1,103 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import getThreads from '@salesforce/apex/CRM_MessageHelper.getThreadsCollection';
+import createThread from '@salesforce/apex/CRM_MessageHelper.createThread';
+import { refreshApex } from '@salesforce/apex';
+
+export default class CrmMessagingMessageComponent extends LightningElement {
+    @api recordId;
+    @api singleThread;
+    @api showClose;
+    @api showQuick;
+    @api englishTextTemplate;
+    @api textTemplate; //Support for conditional text template
+    @api caseId;
+
+    @track slotsNeedCheckedOrRendered = { messages: true }; // To check the slot content the slot has to be rendered initially
+
+    showmodal = false;
+    showtaskmodal = false;
+    activeSectionMessage = '';
+    threads;
+    singlethread;
+    _threadsforRefresh;
+    setCardTitle;
+    hasError = false;
+
+    renderedCallback() {
+        this.handleSlotChanges();
+    }
+
+    @wire(getThreads, { recordId: '$recordId', singleThread: '$singleThread' }) //Calls apex and extracts messages related to this record
+    wiredThreads(result) {
+        this._threadsforRefresh = result;
+
+        if (result.error) {
+            this.error = result.error;
+            this.hasError = true;
+            console.log(JSON.stringify(result.error, null, 2));
+        } else if (result.data) {
+            this.threads = result.data;
+        }
+    }
+    handlenewpressed() {
+        createThread({ recordId: this.recordId })
+            .then((result) => {
+                return refreshApex(this._threadsforRefresh);
+            })
+            .catch((error) => {});
+    }
+
+    get showSpinner() {
+        return !this.threads && !this.hasError;
+    }
+
+    get shownewbutton() {
+        return this.threads && this.threads.length === 0 && this.recordId;
+    }
+
+    get cardTitle() {
+        return this.setCardTitle ?? (this.singleThread === true ? 'Samtale' : 'Samtaler');
+    }
+
+    @api
+    set cardTitle(cardTitle) {
+        this.setCardTitle = cardTitle;
+    }
+
+    handleEnglishEvent(event) {
+        const englishEvent = new CustomEvent('englisheventtwo', {
+            detail: event.detail
+        });
+        this.dispatchEvent(englishEvent);
+    }
+
+    // Make the component check slot content dynamically.
+    // If the slot is not rendered in the DOM we have no way of checking it's content
+    @api
+    checkSlotChange(slotName) {
+        console.log('Api call');
+        console.log(slotName);
+        console.log(this.slotsNeedCheckedOrRendered[slotName]);
+        this.slotsNeedCheckedOrRendered[slotName] = true;
+    }
+
+    handleSlotChanges() {
+        const slots = this.template.querySelectorAll('slot');
+        if (!slots) return;
+        const changeableSlots = Object.keys(this.slotsNeedCheckedOrRendered).filter(
+            (slotValue) => this.slotsNeedCheckedOrRendered[slotValue]
+        );
+        for (const slotName of changeableSlots) {
+            let slot;
+            for (const a of slots) {
+                if (a.name === slotName) {
+                    slot = a;
+                    break;
+                }
+            }
+            if (!slot) continue;
+            const hasContent = slot.assignedElements().length !== 0;
+            this.slotsNeedCheckedOrRendered[slot.name] = hasContent;
+        }
+    }
+}
---
+++ force-app/main/default/internal-components/lwc/crmMessagingThreadViewerV2/crmMessagingThreadViewerV2.js
@@ -0,0 +1,453 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import getmessages from '@salesforce/apex/CRM_MessageHelper.getMessagesFromThread';
+import markAsReadByNav from '@salesforce/apex/CRM_MessageHelper.markAsReadByNav';
+import { subscribe, unsubscribe } from 'lightning/empApi';
+import userId from '@salesforce/user/Id';
+import { updateRecord, getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import ACTIVE_FIELD from '@salesforce/schema/Thread__c.CRM_isActive__c';
+import THREAD_ID_FIELD from '@salesforce/schema/Thread__c.Id';
+import REGISTERED_DATE from '@salesforce/schema/Thread__c.CRM_Date_Time_Registered__c';
+import COMPLETE_LABEL from '@salesforce/label/c.STO_Complete';
+import CREATE_NAV_TASK_LABEL from '@salesforce/label/c.STO_Create_NAV_Task';
+import JOURNAL_LABEL from '@salesforce/label/c.STO_Journal';
+import END_DIALOGUE_LABEL from '@salesforce/label/c.STO_End_Dialogue';
+import SEND_TO_REDACTION_LABEL from '@salesforce/label/c.Set_To_Redaction';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+import { publishToAmplitude } from 'c/amplitude';
+import LoggerUtility from 'c/loggerUtility';
+
+export default class MessagingThreadViewer extends LightningElement {
+    @api recordId;
+    @api thread;
+    @api showQuick;
+    @api englishTextTemplate;
+    @api textTemplate; //Support for conditional text template as input
+    @api showClose;
+
+    @track langBtnLock = false;
+
+    createdbyid;
+    usertype;
+    otheruser;
+    _mySendForSplitting;
+    threadheader;
+    threadId;
+    messages = [];
+    showspinner = false;
+    hideModal = true;
+    langBtnAriaToggle = false;
+    resizablePanelTop;
+    onresize = false; // true when in process of resizing
+    mouseListenerCounter = false; // flag for detecting if onmousemove listener is set for element
+    registereddate;
+    closedThread;
+
+    //Constructor, called onload
+    connectedCallback() {
+        if (this.thread) {
+            this.threadId = this.thread.Id;
+        }
+        this.handleSubscribe();
+        this.scrolltobottom();
+        markAsReadByNav({ threadId: this.threadId });
+    }
+
+    disconnectedCallback() {
+        this.handleUnsubscribe();
+    }
+
+    renderedCallback() {
+        this.scrolltobottom();
+        const test = this.template.querySelector('.cancelButton');
+        if (test) {
+            test.focus();
+        }
+        this.resizablePanelTop = this.template.querySelector('section');
+        this.resizablePanelTop.addEventListener('mousemove', this.mouseMoveEventHandlerBinded, false);
+        this.resizablePanelTop.addEventListener('mouseleave', this.mouseLeaveEventHandler, false);
+    }
+
+    //##################################//
+    //#####    Event Handlers    #######//
+    //##################################//
+
+    mouseMoveEventHandler(e) {
+        // detecting if cursor is in the area of interest
+        if (this.resizablePanelTop.getBoundingClientRect().bottom - e.pageY < 10) {
+            // change cursor style, and adding listener for mousedown event
+            document.body.style.cursor = 'ns-resize';
+            if (this.mouseListenerCounter !== true) {
+                this.resizablePanelTop.addEventListener('mousedown', this.mouseDownEventHandlerBinded, false);
+                this.mouseListenerCounter = true;
+            }
+        } else {
+            // remove listener and reset cursor when cursor is out of area of interest
+            if (this.mouseListenerCounter === true) {
+                this.resizablePanelTop.removeEventListener('mousedown', this.mouseDownEventHandlerBinded, false);
+                this.mouseListenerCounter = false;
+            }
+            document.body.style.cursor = 'auto';
+        }
+    }
+    //binding, to make 'this' available when running in context of other object
+    mouseMoveEventHandlerBinded = this.mouseMoveEventHandler.bind(this);
+
+    mouseLeaveEventHandler(e) {
+        if (this.mouseListenerCounter === true) {
+            this.resizablePanelTop.removeEventListener('mousedown', this.mouseDownEventHandlerBinded, false);
+            this.mouseListenerCounter = false;
+        }
+        document.body.style.cursor = 'auto';
+    }
+
+    mouseDownEventHandler(e) {
+        this.onresize = true;
+        this.resizablePanelTop.removeEventListener('mousedown', this.mouseDownEventHandlerBinded, false);
+        document.addEventListener('mouseup', this.mouseUpEventHandlerBinded, true);
+        this.resizablePanelTop.removeEventListener('mousemove', this.mouseMoveEventHandlerBinded, false);
+        this.resizablePanelTop.removeEventListener('mouseleave', this.mouseLeaveEventHandler, false);
+        document.addEventListener('mousemove', this.resizeEventHandlerBinded, true);
+    }
+    mouseDownEventHandlerBinded = this.mouseDownEventHandler.bind(this);
+
+    resizeEventHandler(e) {
+        e.preventDefault();
+        this.resizablePanelTop.style.height = this.resizablePanelTop.offsetHeight + e.movementY + 'px';
+    }
+    resizeEventHandlerBinded = this.resizeEventHandler.bind(this);
+
+    mouseUpEventHandler(e) {
+        this.onresize = false;
+        this.resizablePanelTop.removeEventListener('mousedown', this.mouseDownEventHandlerBinded, false);
+        document.removeEventListener('mouseup', this.mouseUpEventHandlerBinded, true);
+        document.removeEventListener('mousemove', this.resizeEventHandlerBinded, true);
+        document.body.style.cursor = 'auto';
+        this.resizablePanelTop.addEventListener('mousemove', this.mouseMoveEventHandlerBinded, false);
+        this.resizablePanelTop.addEventListener('mouseleave', this.mouseLeaveEventHandler, false);
+        this.mouseListenerCounter = false;
+    }
+    mouseUpEventHandlerBinded = this.mouseUpEventHandler.bind(this);
+
+    //Handles subscription to streaming API for listening to changes to auth status
+    handleSubscribe() {
+        let _this = this;
+        // Callback invoked whenever a new message event is received
+        const messageCallback = function (response) {
+            const messageThreadId = response.data.sobject.CRM_Thread__c;
+            if (_this.threadId == messageThreadId) {
+                //Refreshes the message in the component if the new message event is for the viewed thread
+                _this.refreshMessages();
+            }
+        };
+
+        // Invoke subscribe method of empApi. Pass reference to messageCallback
+        subscribe('/topic/Thread_New_Message', -1, messageCallback).then((response) => {
+            // Response contains the subscription information on successful subscribe call
+            this.subscription = response;
+        });
+    }
+
+    handleUnsubscribe() {
+        unsubscribe(this.subscription, (response) => {
+            console.log('Unsubscribed: ', JSON.stringify(response));
+            // Response is true for successful unsubscribe
+        })
+            .then((success) => {
+                //Successfull unsubscribe
+            })
+            .catch((error) => {
+                console.log('EMP unsubscribe failed: ' + JSON.stringify(error, null, 2));
+            });
+    }
+
+    @wire(getRecord, {
+        recordId: '$threadId',
+        fields: [ACTIVE_FIELD, REGISTERED_DATE]
+    })
+    wiredThread(resp) {
+        const { data, error } = resp;
+        if (data) {
+            try {
+                this.registereddate = getFieldValue(data, REGISTERED_DATE);
+                const active = getFieldValue(data, ACTIVE_FIELD);
+                this.closedThread = !active;
+            } catch (catchError) {
+                this.doTheLog(catchError, resp);
+            }
+        }
+        if (error) {
+            console.log('Was error');
+            console.log(error);
+            this.doTheLog(null, resp);
+        }
+    }
+
+    @wire(getmessages, { threadId: '$threadId' }) //Calls apex and extracts messages related to this record
+    wiremessages(result) {
+        this._mySendForSplitting = result;
+        if (result.error) {
+            this.error = result.error;
+        } else if (result.data) {
+            this.messages = result.data;
+            this.showspinner = false;
+        }
+    }
+    //If empty, stop submitting.
+    handlesubmit(event) {
+        publishToAmplitude('STO', { type: 'handlesubmit on thread' });
+
+        this.lockLangBtn();
+        event.preventDefault();
+        if (!this.quickTextCmp.isOpen()) {
+            this.showspinner = true;
+            const textInput = event.detail.fields;
+            // If messagefield is empty, stop the submit
+            textInput.CRM_Thread__c = this.thread.Id;
+            textInput.CRM_From_User__c = userId;
+
+            if (textInput.CRM_Message_Text__c == null || textInput.CRM_Message_Text__c === '') {
+                const event1 = new ShowToastEvent({
+                    title: 'Message Body missing',
+                    message: 'Make sure that you fill in the message text',
+                    variant: 'error'
+                });
+                this.dispatchEvent(event1);
+                this.showspinner = false;
+            } else {
+                this.template.querySelector('lightning-record-edit-form').submit(textInput);
+            }
+        }
+    }
+
+    //Enriching the toolbar event with reference to the thread id
+    //A custom toolbaraction event can be passed from the component in the toolbar slot that the thread viewer enrich with the thread id
+    handleToolbarAction(event) {
+        let threadId = this.threadId;
+        let eventDetails = event.detail;
+        eventDetails.threadId = threadId;
+        event.threadId = threadId;
+    }
+
+    closeThread() {
+        publishToAmplitude('STO', { type: 'closeThread' });
+
+        this.closeModal();
+        const fields = {};
+        fields[THREAD_ID_FIELD.fieldApiName] = this.threadId;
+        fields[ACTIVE_FIELD.fieldApiName] = false;
+
+        const threadInput = { fields };
+        this.showspinner = true;
+        updateRecord(threadInput)
+            .then(() => {
+                const event1 = new ShowToastEvent({
+                    title: 'Avsluttet',
+                    message: 'Samtalen ble avsluttet',
+                    variant: 'success'
+                });
+                this.dispatchEvent(event1);
+            })
+
+            .catch((error) => {
+                console.log(JSON.stringify(error, null, 2));
+                const event1 = new ShowToastEvent({
+                    title: 'Det oppstod en feil',
+                    message: 'Samtalen kunne ikke bli avsluttet',
+                    variant: 'error'
+                });
+                this.dispatchEvent(event1);
+            })
+            .finally(() => {
+                this.refreshMessages();
+                this.showspinner = false;
+            });
+    }
+
+    handlesuccess(event) {
+        this.recordId = event.detail;
+
+        this.quickTextCmp.clear();
+        const inputFields = this.template.querySelectorAll('.msgText');
+
+        if (inputFields) {
+            inputFields.forEach((field) => {
+                field.reset();
+            });
+        }
+        //this.showspinner = false;
+        this.showspinner = false;
+        this.refreshMessages();
+    }
+
+    scrolltobottom() {
+        var element = this.template.querySelector('.slds-box');
+        if (element) {
+            element.scrollTop = element.scrollHeight;
+        }
+    }
+    refreshMessages() {
+        return refreshApex(this._mySendForSplitting);
+    }
+
+    showQuickText(event) {
+        publishToAmplitude('STO', { type: 'showQuickText' });
+        this.quickTextCmp.showModal(event);
+    }
+
+    handleLangClick() {
+        publishToAmplitude('STO', { type: 'handleLangClick' });
+        const englishEvent = new CustomEvent('englishevent', {
+            detail: !this.englishTextTemplate
+        });
+        this.langBtnAriaToggle = !this.langBtnAriaToggle;
+        this.dispatchEvent(englishEvent);
+    }
+
+    lockLangBtn() {
+        this.langBtnLock = true;
+    }
+
+    //##################################//
+    //#########    GETTERS    ##########//
+    //##################################//
+
+    get quickTextCmp() {
+        return this.template.querySelector('c-crm-messaging-quick-text-v-2');
+    }
+
+    get text() {
+        return this.quickTextCmp ? this.quickTextCmp.conversationNote : '';
+    }
+
+    get modalClass() {
+        return 'slds-modal slds-show uiPanel north' + (this.hideModal === true ? ' geir' : ' slds-fade-in-open');
+    }
+
+    get backdropClass() {
+        return this.hideModal === true ? 'slds-hide' : 'backdrop';
+    }
+
+    get langBtnVariant() {
+        return this.englishTextTemplate === false ? 'neutral' : 'brand';
+    }
+
+    get langAria() {
+        return this.langBtnAriaToggle === false ? 'Språk knapp, Norsk' : 'Språk knapp, Engelsk';
+    }
+
+    get hasEnglishTemplate() {
+        return this.englishTextTemplate !== undefined;
+    }
+
+    //##################################//
+    //########    MODAL    #############//
+    //##################################//
+
+    openModal() {
+        publishToAmplitude('STO', { type: 'openModal close thread' });
+        this.hideModal = false;
+    }
+
+    closeModal() {
+        publishToAmplitude('STO', { type: 'closeModal close thread' });
+        this.hideModal = true;
+    }
+
+    trapFocusStart() {
+        const firstElement = this.template.querySelector('.closeButton');
+        firstElement.focus();
+    }
+
+    trapFocusEnd() {
+        const lastElement = this.template.querySelector('.cancelButton');
+        lastElement.focus();
+    }
+
+    //##################################//
+    //##########   Logging   ###########//
+    //##################################//
+
+    doTheLog(error, response) {
+        const report = `Error: ${error}, response: ${JSON.stringify(response)}`;
+        LoggerUtility.logError(
+            'NKS',
+            'STO',
+            report,
+            'Could not fetch active field from thread internal view',
+            this.threadId
+        );
+    }
+
+    //##################################//
+    //##########   STO Button Container   ###########//
+    //##################################//
+    @api caseId;
+
+    complete = COMPLETE_LABEL;
+    createNavTask = CREATE_NAV_TASK_LABEL;
+    journal = JOURNAL_LABEL;
+    endDialogue = END_DIALOGUE_LABEL;
+    redact = SEND_TO_REDACTION_LABEL;
+
+    showFlow = false;
+    showComplete = false;
+    showRedact = false;
+    showJournal = false;
+    showCreateNavTask = false;
+    label;
+
+    get inputVariables() {
+        return [
+            {
+                name: 'recordId',
+                type: 'String',
+                value: this.caseId
+            }
+        ];
+    }
+
+    toggleFlow(event) {
+        this.showFlow = !this.showFlow;
+        this.label = event.target.label;
+        this.handleShowFlows();
+        publishToAmplitude('STO', { type: this.label + ' pressed' });
+    }
+
+    handleShowFlows() {
+        if (this.label === this.complete) {
+            this.showComplete = true;
+            this.showRedact = false;
+            this.showJournal = false;
+            this.showCreateNavTask = false;
+        }
+
+        if (this.label === this.redact) {
+            this.showRedact = true;
+            this.showComplete = false;
+            this.showJournal = false;
+            this.showCreateNavTask = false;
+        }
+
+        if (this.label === this.createNavTask) {
+            this.showCreateNavTask = true;
+            this.showRedact = false;
+            this.showComplete = false;
+            this.showJournal = false;
+        }
+
+        if (this.label === this.journal) {
+            this.showJournal = true;
+            this.showCreateNavTask = false;
+            this.showRedact = false;
+            this.showComplete = false;
+        }
+    }
+
+    handleStatusChange(event) {
+        let flowStatus = event.detail.status;
+        if (flowStatus === 'FINISHED' || flowStatus === 'FINISHED_SCREEN') {
+            this.showFlow = false;
+        }
+    }
+}
